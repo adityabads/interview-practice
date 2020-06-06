10a267702e5d6df4bb2ee935075d52b97f02c1f1
Kevin Chen <kevin.chen.krc@gmail.com>
2020-06-06 15:48:27 -0400

    Finish reverse string

---
+++ 1-arrays-strings/2_reverse_string.py
@@ -0,0 +1,29 @@
+# Reverse string
+# Write a function that takes a list of characters and reverses the letters in place.
+
+from typing import List
+import unittest
+
+
+def reverse_string(s: List[str]) -> None:
+    i = 0
+    j = len(s) - 1
+    while i < j:
+        s[i], s[j] = s[j], s[i]
+        i += 1
+        j -= 1
+
+
+class TestReverseString(unittest.TestCase):
+    def test_reverse_string(self):
+        tests = [
+            [val for val in "abcdefghij"[:j]] for j in range(10)
+        ]
+        for test in tests:
+            reversedstr = test.copy()
+            reverse_string(reversedstr)
+            self.assertEqual(reversedstr, list(reversed(test)))
+
+
+if __name__ == "__main__":
+    unittest.main()
