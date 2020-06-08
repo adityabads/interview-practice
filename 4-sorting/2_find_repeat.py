f0a04375d23b648479ba1fee3625e605b14ac832
Kevin Chen <kevin.chen.krc@gmail.com>
2020-06-08 17:47:58 -0400

    Correct solution

--- 4-sorting/2_find_repeat.py
+++ 4-sorting/2_find_repeat.py
@@ -8,6 +8,10 @@
 # (If there are multiple duplicates, you only need to find one of them.)
 #
 # Optimize for space.
+#
+# BONUS
+# This function always returns one duplicate, but there may be several duplicates.
+# Write a function that returns all duplicates.
 
 from typing import List
 import unittest
@@ -15,29 +19,27 @@ import unittest
 
 def find_repeat(arr: List[int], maxval: int) -> int:
     """Returns a repeated element in `arr`"""
-    i = 0
+    i = 1
     j = maxval
-    while True:
+    while i < j:
         mid = (i+j)//2
-        countfirsthalf = countsecondhalf = 0
+        countfirsthalf = 0
         for val in arr:
-            if val < mid:
+            if i <= val and val <= mid:
                 countfirsthalf += 1
-            else:
-                countsecondhalf += 1
-        if countfirsthalf > mid - i:
+        if countfirsthalf > mid - i + 1:
             j = mid
         else:
             i = mid+1
-        if i+1 == j:
-            return i
+    return i
 
 
 class TestFindRepeat(unittest.TestCase):
     def test_find_repeat(self):
         tests = [
-            [[1, 4, 7, 3, 9, 7], 9, [7]],
-            [[2, -1, 0, -1, 2], 2, [-1, 2]]
+            [[1, 4, 2, 5, 7, 8, 6, 3, 9, 7], 9, [7]],
+            [[2, 1, 2], 2, [2]],
+            [[6, 4, 3, 5, 5, 3, 1], 6, [3, 5]]
         ]
         for arr, maxval, expectedvals in tests:
             self.assertIn(find_repeat(arr, maxval), expectedvals)
