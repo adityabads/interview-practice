836941701eaaaf2810d4d619315081b13d6a15a2
Kevin Chen <kevin.chen.krc@gmail.com>
2020-06-08 13:54:04 -0400

    Finish product of all other numbers

--- 3-greedy-algos/2_product_of_three.py
+++ 3-greedy-algos/2_product_of_three.py
@@ -1,4 +1,4 @@
-# Product of three
+# Highest product of three
 # Given a list of integers, find the highest product you can get from three
 # of the integers.
 #
---
+++ 3-greedy-algos/3_product_all_others.py
@@ -0,0 +1,56 @@
+# Product of all other numbers
+# You have a list of integers, and for each index you want to find the product
+# of every integer except the integer at that index.
+#
+# Write a function get_products_of_all_ints_except_at_index() that takes a list
+# of integers and returns a list of the products.
+#
+# EXAMPLE
+# Input:  [1, 7, 3, 4]
+# Output: [7*3*4, 1*3*4, 1*7*4, 1*7*3]
+#
+# Here's the catch: You can't use division in your solution!
+
+from typing import List
+import unittest
+
+
+def get_all_products(arr: List[int]) -> None:
+    """Return answer in place"""
+    n = len(arr)
+    if n < 2:
+        raise Exception("Input requires at least two ints")
+
+    # rightprods[i] := product(arr[j]) s.t. j > i
+    rightprods = [0] * n
+    rightprods[-1] = 1
+    for i in reversed(range(n-1)):
+        rightprods[i] = rightprods[i+1] * arr[i+1]
+
+    # turn arr into leftprods[i] := product(arr[j]) s.t. j < i
+    leftprodval = arr[0]
+    arr[0] = 1
+    for i in range(1, n):
+        val = arr[i]
+        arr[i] = leftprodval
+        leftprodval *= val
+
+    # answer is leftprods[i] * rightprods[i] = product(arr[j]) s.t. i != j
+    for i in range(n):
+        arr[i] *= rightprods[i]
+
+
+class TestProductAllOthers(unittest.TestCase):
+    def test_get_all_products(self):
+        tests = [
+            [[1, 7, 3, 4], [7*3*4, 1*3*4, 1*7*4, 1*7*3]],
+            [[2, 9, 5, 7, 8], [9*5*7*8, 2*5*7*8, 2*9*7*8, 2*9*5*8, 2*9*5*7]],
+            [[2, 0, 7, 8], [0, 2*7*8, 0, 0]]
+        ]
+        for test, expected in tests:
+            get_all_products(test)
+            self.assertEqual(test, expected)
+
+
+if __name__ == "__main__":
+    unittest.main()
